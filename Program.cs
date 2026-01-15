c3636d19531028a370f8f5313526368db7f273e1
mantoangabe <35808413+mantoangabe@users.noreply.github.com>
2026-01-15 11:30:59 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Boolean programLooper = true;
+while (programLooper == true)
+{
+    Console.WriteLine("Welcome to Food Bank Inventory Program!");
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine("1. Add Food Items");
+    Console.WriteLine("2. Delete Food Items");
+    Console.WriteLine("3. View Food Items");
+    Console.WriteLine("4. Exit");
+    Console.Write("Please enter your choice: (1-4)");
+    if (!Int32.TryParse(Console.ReadLine(), out int userInput))
+    {
+        Console.WriteLine("Invalid input, please try again");
+        continue;
+    }
+
+    switch (userInput)
+    {
+        case 1:
+            //Add class
+            break;
+        case 2:
+            //delete class
+            break;
+        case 3:
+            //View food items
+            break;
+        case 4:
+            programLooper = false;
+            Console.WriteLine("Goodbye!");
+            break;
+        default:
+            Console.WriteLine("Please choose a valid option. \n");
+            break;
+            
+    }
+    
+}
